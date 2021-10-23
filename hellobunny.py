a28b906bc308f6bf25053f5235fd38a00ccede46
Sameer Thapa <93032227+Jackthapa@users.noreply.github.com>
2021-10-23 14:45:41 +0545

    Hello Bunny simulation in python

---
+++ hellobunny.py
@@ -0,0 +1,27 @@
+# 1 - Import library
+import pygame
+from pygame.locals import *
+
+# 2 - Initialize the game
+pygame.init()
+width, height = 640, 480
+screen=pygame.display.set_mode((width, height))
+
+# 3 - Load images
+player = pygame.image.load("resources/images/dude.png")
+
+# 4 - keep looping through
+while 1:
+    # 5 - clear the screen before drawing it again
+    screen.fill(0)
+    # 6 - draw the screen elements
+    screen.blit(player, (100,100))
+    # 7 - update the screen
+    pygame.display.flip()
+    # 8 - loop through the events
+    for event in pygame.event.get():
+        # check if the event is the X button 
+        if event.type==pygame.QUIT:
+            # if it is quit the game
+            pygame.quit() 
+            exit(0) 
