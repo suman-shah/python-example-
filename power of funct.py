451f878450e6a6cf975ee7728b8fddafb3cb647c
yadwvai <114802213+yadwvai@users.noreply.github.com>
2022-10-01 14:05:20 +0545

    Create power of funct.py

---
+++ power of funct.py
@@ -0,0 +1,8 @@
+number = int(input(" Please Enter any Positive Integer : "))
+exponent = int(input(" Please Enter Exponent Value : "))
+power = 1
+
+for i in range(1, exponent + 1):
+    power = power * number
+    
+print("The Result of {0} Power {1} = {2}".format(number, exponent, power))
