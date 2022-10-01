39da05386765f2561b1c1c8bc6a81bd098ff0520
suman shah <114487868+juwnathd@users.noreply.github.com>
2022-10-01 12:53:54 +0545

    Python Calendar Example

---
+++ Python Calendar.py
@@ -0,0 +1,7 @@
+import calendar
+
+# ask of month and year
+year = int(input("Please Enter the year Number: "))
+month = int(input("Please Enter the month Number: "))
+
+print(calendar.month(year, month))
