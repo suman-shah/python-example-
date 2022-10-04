f68f5dabb6c13fd00c4fd2244c0fdd932b612467
Vaibahuraja <115003331+Vaibahuraja@users.noreply.github.com>
2022-10-04 19:02:20 +0545

    Create RegEx.py

---
+++ RegEx.py
@@ -0,0 +1,11 @@
+import re
+
+#Check if the string starts with "The" and ends with "Spain":
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+if x:
+  print("YES! We have a match!")
+else:
+  print("No match")
